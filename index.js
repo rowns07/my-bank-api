a0a825041803ffa089c1bb758cd436cfa65de8b5
Diego Souza <diegosouza@bestpontos.com.br>
2020-04-29 21:04:31 -0300

    Commit Inicial

---
+++ index.js
@@ -0,0 +1,9 @@
+const app = require('express')();
+
+app.get('/', function (req, res) {
+    res.send('Hello Worldinho!');
+});
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
